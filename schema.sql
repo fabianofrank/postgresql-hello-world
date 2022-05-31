b63deb4aad096c7273c35321a71264e772dc6dee
fabianofrank <fabianofrank@protonmail.com>
2022-05-31 16:56:53 -0300

    Join tables and more complex queries

--- schema.sql
+++ schema.sql
@@ -36,14 +36,39 @@ ALTER TABLE animals
 DROP COLUMN species;
 
 /* Cleaner Key - Adds sequence column for the FK automatically */
+
 ALTER TABLE animals
 ADD species_id INT
 REFERENCES species(id);
 
 /* Step-by-step Key - Adds sequence column for the FK manually */
+
 ALTER TABLE animals
 ADD COLUMN owners_id INT;
 ALTER TABLE animals
 ADD CONSTRAINT owners_key
   FOREIGN KEY (owners_id)
     REFERENCES owners(id);
+
+/* Vets, Specializations and Visits Tables (M-to-M) */
+
+CREATE TABLE vets (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  age INT NOT NULL,
+  date_of_graduation DATE NOT NULL
+);
+
+CREATE TABLE specializations (
+    species_id INT REFERENCES species(id),
+    vets_id INT REFERENCES vets(id),
+    PRIMARY KEY (species_id, vets_id)
+);
+
+CREATE TABLE visits (
+    animals_id INT,
+    vets_id INT,
+    date_of_visit DATE NOT NULL,
+    FOREIGN KEY (animals_id) REFERENCES animals(id),
+    FOREIGN KEY (vets_id) REFERENCES vets(id)
+);
